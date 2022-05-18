11b7d2aef85d595214d5e166957ef39d24adfb51
jeelhmalaviya <105771087+jeelhmalaviya@users.noreply.github.com>
2022-05-18 11:25:39 +0530

    Add files via upload

---
+++ h1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+       int i;
+       cout<<"enter any number";
+       cin>>i;
+       if(i>=90)
+       {
+          cout<<"1st class";
+       }
+          else if(i>=80)
+          {
+             cout<<"2nd class";
+          }
+               else if(i>=60)
+               {
+                 cout<<"3rd class";
+               }
+                  else if(i>=40)
+                  {
+                    cout<<"fail";
+                  }
+}
+
